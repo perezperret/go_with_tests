0535fc4eb053c61be1fd87e51e385fbdf1397750
Santiago Perez Perret <perezperret@gmail.com>
2022-09-06 17:28:46 -0400

    First version of repeat

---
+++ iteration/iteration_test.go
@@ -0,0 +1,27 @@
+package iteration
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func TestRepeatWithCount(t *testing.T) {
+	repeated := Repeat("b", 10)
+	expected := "bbbbbbbbbb"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
-https://quii.gitbook.io/learn-go-with-tests/
+# My solutions for [Learn Go with tests](https://quii.gitbook.io/learn-go-with-tests/)
+
+- [X] Hello, World
+- [X] Integers
+- [ ] Iteration
+- [ ] Arrays and slices
+- [ ] Structs, methods and interfaces
